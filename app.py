07835c50231d3965f4a562e708222db4258e6eac
Jonathan Nhouyvanisvong <jonathann.pro@yahoo.com>
2022-04-12 14:53:25 +0200

    reajust some lines

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # Import
+from matplotlib.font_manager import get_fontconfig_fonts
 import streamlit as st
 import datetime
 import numpy as np
@@ -30,7 +31,7 @@ h1 {
 """
 st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
 
-# st.dataframe(my_dataframe)
+
 expander_df = st.expander(label='DataFrame')
 
 with expander_df:
@@ -42,29 +43,36 @@ with expander_df:
 
     df = get_cached_data()
 
-    option_head = st.slider('head : ', 1, 20000, 5)
+    option_head = st.slider('head : ', 1, 1000, 5)
     st.write(df.head(option_head))
 
 st.write('Min date: ', min(df['date']))
 st.write('Max date: ', max(df['date']))
 
+
+
+
 # Inventory Units (With + or -) %
 # Sales Units (With + or -) %
 col1, col2 = st.columns(2)
 col1.metric("Inventory Units", "437.8", "-$1.25")
+
+# show family_sales for this month
+# compare previous months
+# df['family_sales']
+# df.loc[df['date'].dt.year == ]
+
 col2.metric("Sales Units", "121.10", "0.46%")
 
 
 
 with st.sidebar:
-
     '''
     # Forecast for sales
 
     ## Management inventory
     '''
-    show_plot = st.checkbox('Show Plot')
-    '--------'
+    '------------------------'
     '''
     - Inventory days of supply
     - Product stock details
@@ -94,53 +102,34 @@ with st.sidebar:
     '''
     mapping = st.checkbox('Show Map')
 
-if show_plot:
-    # Test directly plot
-    # ------------------
-    @st.cache
-    def countplot_temp():
-        # fig , axes = plt.subplots(2, 2, figsize=(15, 16))
-        fig , axes = plt.subplots(1, 2, figsize=(15, 16))
+# if show_plot:
+#     # Test directly plot
+#     # ------------------
+#     @st.cache
+#     def countplot_temp():
+#         # fig , axes = plt.subplots(2, 2, figsize=(15, 16))
+#         fig , axes = plt.subplots(1, 2, figsize=(15, 16))
 
-        sns.countplot(ax=axes[0], data=df, y='family',
-                    order=df['family'].value_counts().index)\
-                        .set_title('Countplot family of products')
+#         sns.countplot(ax=axes[0], data=df, y='family_sales',
+#                     order=df['family'].value_counts().index)\
+#                         .set_title('Countplot family of products')
 
-        # sns.countplot(ax=axes[0, 1], data=df, y='type_x',
-        #             order=df['type_x'].value_counts().index)\
-        #                 .set_title('Countplot type of products')
+#         return fig
 
-        # sns.countplot(ax=axes[1, 0], data=df, y='state',
-        #             order=df['state'].value_counts().index)\
-        #                 .set_title('Countplot nb products by state')
-
-        # sns.histplot(ax=axes[1],data=df, x='family_sales')\
-        #     .set_title('Countplot onpromotion')
-
-        return fig
-
-    st.pyplot(countplot_temp())
+#     st.pyplot(countplot_temp())
 
 
-# fig2 = px.bar(df[df['store_nbr'] == 25], x='date', y='family_sales')
-# st.pyplot()
-
-# ------------------
 
 col_show1, col_show2 = st.columns(2)
-# Checkbox to display something
-# - Inventory days of supply
-# - Product stock details
-# - Inventory Trend
-# - Inventory Efficient (Lines predict, Lines real)
 with col_show1: # Columns 1
-    if show_1: # InvDaysSupply,
+    if show_1: # InvDaysSupply, ProdStockDetails
         '''
         ### Screen 1 : Inventory days of supply
         '''
-        # Plot with confidence interval
+        # Plot with confidence interval - start
 
 
+        # Plot with confidence interval - end
         '''
         ### Screen 1 : Product stock details
         '''
@@ -148,13 +137,17 @@ with col_show1: # Columns 1
         # [product, date, unit_hand, unit_order]
         # if family_sales actual > or <, print 🔻 or ⬆
 
-        df_stock_details = df[['family', 'family_sales']]\
+        df_stock_details = df[['family', 'date', 'family_sales']]\
                             .groupby(by='family').sum()
+
+        # df_stock_details['date'] = pd.to_datetime(df_stock_details['date'])
+
+        # if df_stock_details['date'] ==
 
         st.dataframe(df_stock_details)
 
 
-    if show_3:
+    if show_3: # InvTrend, InvEff (Pred, Real)
         '''
         ### Screen 3 : Inventory Trend
         '''
@@ -171,7 +164,7 @@ with col_show1: # Columns 1
 with col_show2: # Column 2
     if show_2: # top10, NeedProduct
         '''
-        ### Screen 2 : Top 10 of sales, invetories
+        ### Screen 2 : Top 10 of sales, inventories
         '''
         # add date to choose
         sb_year_top_10 = st.selectbox('Year',
@@ -206,10 +199,11 @@ with col_show2: # Column 2
         '''
         sb_family = st.selectbox('Family', df['family'].unique())
         df_family = df[df['family'] == sb_family]
+
         # st.dataframe(df_family[['item_nbr', 'unit_sales']])
         # Better with barplots
 
-        st.plotly_chart(px.bar(df, y='family'))
+        # st.plotly_chart(px.bar(df, y='item_nbr'))
 
         # days with sliders, textarea
         '''
@@ -217,7 +211,8 @@ with col_show2: # Column 2
         '''
 
 
-        df_expire = df.loc[df['family_sales'] != 0].loc[df['family_sales'] <= 10]
+        df_expire = df.loc[df['family_sales'] != 0]\
+                        .loc[df['family_sales'] <= 10][['family', 'family_sales']]
 
         st.dataframe(df_expire)
         # With selectbox of expire days (10, 9, ...)
@@ -225,7 +220,7 @@ with col_show2: # Column 2
 
 if st.checkbox('Inv. efficient'):
     '''
-    ### Screen 3 : Inventory Efficient (Lines predict, Lines real)
+    ### Screen down : Inventory Efficient (Lines predict, Lines real)
     '''
 
     # def display_time_series_2():
@@ -235,32 +230,36 @@ if st.checkbox('Inv. efficient'):
     #     fig = px.line(df, x='date', y='family_sales', markers=True)
     #     return fig
 
+    # Just remove after
     st.write(df.groupby(by='family').sum().sort_values('family_sales', ascending=False))
 
+
     # df.loc[df['family'] == 'GROCERY I'].loc[df['date'].dt.year == 2015]
-
-
     @st.cache
     def inv_efficient_plotly(df):
         # see after to compare predict/real
         # fig = px.line(df, x="year", y="lifeExp", color='country')
         # GROCERY I
+
+        
         df['date'] = pd.to_datetime(df['date'])
         df_one_family = df.loc[df['family'] == 'GROCERY I']\
                             .loc[df['date'].dt.year == 2015]
         df_second_family = df.loc[df['family'] == 'BEVERAGES']\
                                 .loc[df['date'].dt.year == 2015]
+        df_compare = pd.concat([df_one_family, df_second_family])
 
-        fig_one = px.line(df_one_family, x='date', y='family_sales', markers=True,
-                          title='Inventory Efficient of application')
-        fig_two = px.line(df_second_family, x='date', y='family_sales', markers=True)
+        # later, color to diff predict with real
+        fig_one = px.line(df_compare, x='date', y='family_sales', markers=True,
+                          title='Inventory Efficient of application', color='family')
 
-        return fig_one, fig_two
-    fig_one, fig_two = inv_efficient_plotly(df)
+        fig_one.update_layout(paper_bgcolor='#B2B1B9')
+        # fig_two = px.line(df_second_family, x='date', y='family_sales', markers=True)
+
+        return fig_one
+    fig_one = inv_efficient_plotly(df)
 
     st.plotly_chart(fig_one)
-    st.plotly_chart(fig_two)
-
 
 
 
